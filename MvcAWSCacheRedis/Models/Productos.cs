491274e3520860dd427252ef9cb62d36789f062d
Estebanmq <esteban.mq09@gmail.com>
2022-05-06 10:56:09 +0200

    Add project files.

---
+++ MvcAWSCacheRedis/Models/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAWSCacheRedis.Models
+{
+    public class Productos
+    {
+        public int IdProducto { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Precio { get; set; }
+
+
+        public string Imagen { get; set; }
+    }
+}
