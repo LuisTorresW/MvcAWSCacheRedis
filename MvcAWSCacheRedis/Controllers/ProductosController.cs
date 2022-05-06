4b519e11670e89edd7f692d4a75532acf8bb6af9
Estebanmq <esteban.mq09@gmail.com>
2022-05-06 13:03:27 +0200

    Commit

--- MvcAWSCacheRedis/Controllers/ProductosController.cs
+++ MvcAWSCacheRedis/Controllers/ProductosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcAWSCacheRedis.Models;
 using MvcAWSCacheRedis.Repositories;
+using MvcAWSCacheRedis.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,11 @@ namespace MvcAWSCacheRedis.Controllers
     public class ProductosController : Controller
     {
         private RepositoryProductos repo;
+        private ServiceCacheAws service;
 
-        public ProductosController(RepositoryProductos repo)
+        public ProductosController(RepositoryProductos repo,ServiceCacheAws service)
         {
+            this.service = service;
             this.repo = repo;
         }
 
@@ -28,6 +31,28 @@ namespace MvcAWSCacheRedis.Controllers
         {
             Productos pro = this.repo.FindProducto(id);
             return View(pro);
+        }
+
+        public IActionResult SeleccionarFavorito(int id)
+        {
+            //BUSCAMOS EL PRODUCTO A ALMACENAR DENTRO DEL REPO
+            Productos productos = this.repo.FindProducto(id);
+            //almacenamos el producto en la cache
+            this.service.AddProducto(productos);
+            TempData["MENSAJE"] = "Producto" + productos.Nombre + "almacenado como Favorito";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Favoritos()
+        {
+            List<Productos> productos = this.service.GetProductosCache();
+            return View(productos);
+        }
+
+        public IActionResult EliminarFavoritos(int id)
+        {
+            this.service.EliminarProductoCache(id);
+            return RedirectToAction("Favoritos");
         }
     }
 }
---
+++ MvcAWSCacheRedis/Services/ServiceCacheAws.cs
@@ -0,0 +1,81 @@
+﻿using MvcAWSCacheRedis.Helpers;
+using MvcAWSCacheRedis.Models;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAWSCacheRedis.Services
+{
+    public class ServiceCacheAws
+    {
+        private IDatabase cache;
+
+        public ServiceCacheAws()
+        {
+            this.cache = CacheRedisMultiplexer.Connection.GetDatabase();
+        }
+
+        // VAMOS A TENER UN METODO PARA ALMACENAR PRODUCTOS EN CACHE
+        //DENTRO DEL CACHE, LO QUE ALMACENAREMOS SERÁ UNA COLECCIÓN DE PRODUCTOS EN FORMATO JSON
+
+        public void AddProducto(Productos producto)
+        {
+            List<Productos> productos;
+            string json = this.cache.StringGet("productoscache");
+            if (json == null)
+            {
+                productos = new List<Productos>();
+            }
+            else
+            {
+                productos = JsonConvert.DeserializeObject<List<Productos>>(json);
+            }
+            productos.Add(producto);
+            //vamos a serializar
+
+            json = JsonConvert.SerializeObject(productos);
+            //almacenamos en cache redis
+            this.cache.StringSet("productoscache", json, TimeSpan.FromMinutes(30));
+
+        }
+
+        public List<Productos> GetProductosCache()
+        {
+            string json = this.cache.StringGet("productoscache");
+            if (json == null)
+            {
+                return null;
+            }
+            else
+            {
+                List<Productos> productos = JsonConvert.DeserializeObject<List<Productos>>(json);
+                return productos;
+            }
+        }
+
+        public void EliminarProductoCache(int idProducto)
+        {
+            //RECUPERAMOS TODOS LOS PRODUCTOS
+            List<Productos> productos = this.GetProductosCache();
+            if (productos != null)
+            {
+                Productos producto = productos.SingleOrDefault(x => x.IdProducto == idProducto);
+                productos.Remove(producto);
+                //si no hay productos eliminamos la clave
+                if (productos.Count() == 0)
+                {
+                    this.cache.KeyDelete("productoscache");
+                }
+                else
+                {
+                    //Serializamos y almacenamos los datos sin el producto eliminado
+                    String json = JsonConvert.SerializeObject(productos);
+                    this.cache.StringSet("productoscache", json, TimeSpan.FromMinutes(30));
+                }
+            }
+        }
+    }
+}
